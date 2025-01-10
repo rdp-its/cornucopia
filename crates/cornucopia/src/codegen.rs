f99b25f738eb14fedfe28205240b9ef0fb27193f
RizkiDPrast <rizkidwip@gmail.com>
2025-01-10 16:28:08 +0700

    add missing semicolon

--- crates/cornucopia/src/codegen.rs
+++ crates/cornucopia/src/codegen.rs
@@ -776,7 +776,7 @@ pub(crate) fn generate(preparation: Preparation, settings: CodegenSettings) -> S
                     move |w: &mut String| {
                         let ctx = GenCtx::new(depth, is_async, settings.derive_ser);
                         let import = if is_async {
-                            "use futures::{StreamExt, TryStreamExt};use futures; use cornucopia_async::GenericClient; use simbe_derive::AxumHandlers"
+                            "use futures::{StreamExt, TryStreamExt};use futures; use cornucopia_async::GenericClient; use simbe_derive::AxumHandlers;"
                         } else {
                             "use postgres::{fallible_iterator::FallibleIterator,GenericClient};"
                         };
